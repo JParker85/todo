828faa440ac4716391150f67cc20f6724cb940ad
jparker85 <jparker85@gmail.com>
2014-07-15 00:08:49 -0400

    the Todo list is displayed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,67 @@
-console.log('The Iron Yard Rocks');
+// Model for the field that will be create the todo list for the app
+
+var Todo = Backbone.Model.extend({
+  defaults: function() {
+    return {
+    task: 'What do you need to do?'
+    }
+  },
+  urlRoot:'http://tiny-pizza-server.herokuapp.com/collections/big',
+  idAttribute: '_id'
+});
+
+
+var TodoList = Backbone.Collection.extend({
+    model: Todo,
+    url:'http://tiny-pizza-server.herokuapp.com/collections/big'
+  });
+
+
+var todoList = new TodoList();
+todoList.fetch();
+
+
+
+var TodoView = Backbone.View.extend({
+  className : 'todo_list',
+
+  initialize: function(){
+      console.log("What's your Big Todo?");
+      // 4 Listens for the event of add in the todoView
+      //http://backbonejs.org/#Events-listenTo
+      this.listenTo(this.collection, 'add', this.render);
+      this.collection.fetch();
+    },
+
+    render: function(){
+      //5 Identifing the HTML source for the template
+      var source = $('#todo_template').html();
+      //6 compiling to the template
+      var template = Handlebars.compile(source);
+      //7 rendering tthe template to the HTML
+      var rendered = template({todoList: this.collection.toJSON()});
+      this.$el.html(rendered);
+      return this;
+  }
+
+});
+
+var todoView = new TodoView ({
+  collection: todoList
+});
+
+
+$(document).ready(function() {
+    $('.todo_list').append(todoView.render().$el);
+    // 1. Wait for the submit button is presedded
+    $('#add_task').submit(function(event){
+        var todo = new Todo();
+        // 2 Set and save the task in the todo on the server
+        todo.set('task', $('#new_task').val());
+        todo.save();
+        // 3 add it to the list
+        todoList.add(todo);
+        return false;
+      });
+ })
+//task: $('#new_task').val()
