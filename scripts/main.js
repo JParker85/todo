518a42988ea68d7061a0fb567951ca88ceddaaa3
jparker85 <jparker85@gmail.com>
2014-07-16 00:11:30 -0400

    gh-pages

--- scripts/main.js
+++ scripts/main.js
@@ -4,32 +4,16 @@ var Todo = Backbone.Model.extend({
   defaults: function() {
     return {
     task: 'What do you need to do?'
-  };
-},
-
+    }
+  },
   urlRoot:'http://tiny-pizza-server.herokuapp.com/collections/big',
-
   idAttribute: '_id'
 });
 
 
 var TodoList = Backbone.Collection.extend({
     model: Todo,
-    url:'http://tiny-pizza-server.herokuapp.com/collections/big',
-    // comparator: -'_id'
-    // this is a completely ridiculously way to force a sort that returns the latest submission first; converts _id from hexadecimal to decimal and sorts by negative, so higher _id values are shown first; since no _id has yet been generated for the new item, force a very large negative number to be returned
-    comparator : function (model) {
-      var pseudoID;
-      if (model.get('_id') == undefined) {
-        pseudoID = -9.5923158918808516e+28;
-        console.log('here');
-      }
-      else {
-        pseudoID = -(parseInt((model.get('_id')), 16));
-      }
-      console.log(pseudoID);
-      return pseudoID;
-    }
+    url:'http://tiny-pizza-server.herokuapp.com/collections/big'
   });
 
 
@@ -43,14 +27,19 @@ var TodoView = Backbone.View.extend({
 
   initialize: function(){
       console.log("What's your Big Todo?");
+      // 4 Listens for the event of add in the todoView
+      //http://backbonejs.org/#Events-listenTo
       this.listenTo(this.collection, 'add', this.render);
       this.collection.fetch();
     },
 
     render: function(){
-      var source = $('#todo_template').html();
+      //5 Identifing the HTML source for the template
+      var source = $('.todo_template').html();
+      //6 compiling to the template
       var template = Handlebars.compile(source);
-      var rendered = template({todoView: this.collection.toJSON()});
+      //7 rendering tthe template to the HTML
+      var rendered = template({todoList: this.collection.toJSON()});
       this.$el.html(rendered);
       return this;
   }
@@ -64,10 +53,29 @@ var todoView = new TodoView ({
 
 $(document).ready(function() {
     $('.todo_list').append(todoView.render().$el);
-    $('#add_task').submit(function(ev){
-        var todo = new Todo({summary: $('#new_task').val()});
-        todo.save(null, {wait: true});
-        todoView.add(todo);
-        return false;
+    // 1. Wait for the submit button is presedded
+    $('.add_task').submit(function(event){
+        var todo = new Todo(),
+          $newTask = $('.new_task');
+        // 2 Set and save the task in the todo on the server
+        todo.set('task', $newTask.val());
+        todo.save();
+        // 3 add it to the list
+        todoList.add(todo);
+        $newTask.val('');
+        event.preventDefault();
+
       });
+
+    $('.todo_list').on('click','.removeTask',function() {
+      var $this = $(this),
+        id = $this.data('id'),
+        todo = todoList.get(id);
+
+      $this.parent().remove();
+
+      todo.destroy();
+    });
+
+
  })
